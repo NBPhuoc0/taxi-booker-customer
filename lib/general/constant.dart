9153ad952084698fe4f946e27cb2f9a04f631392
Nguyễn Bá Phước <phcnguyenba@gmail.com>
2024-05-09 08:56:22 +0700

    update

--- lib/general/constant.dart
+++ lib/general/constant.dart
@@ -1,5 +1,8 @@
-const String headPoint = "https://gop-taxi.azurewebsites.net";  // Giả lập xãi địa chỉ IP riêng thay vì localhost
+const String headPoint =
+    "https://api-gateway.purplesand-fad3fa4f.southeastasia.azurecontainerapps.io";
 
+// const String headPoint =
+//     "http://192.168.0.112:8080"; // Giả lập xãi địa chỉ IP riêng thay vì localhost
 
 class Auth {
   // static const String port = "3001";
@@ -29,24 +32,21 @@ class Customer {
   static const String userInfo = "$headPoint/users/userInfor";
   static const String setDriverRate = "$headPoint/users/rateDriver";
   static const String getDriverRate = "$headPoint/users/driverRate";
-  static const String getDriverLocation = "$headPoint/users/getDriverLocationByBR";
+  static const String getDriverLocation =
+      "$headPoint/users/getDriverLocationByBR";
   static const String cancelRequest = "$headPoint/users/cancelOrder";
 }
 
-
-
 // String key = "AIzaSyBEIQrm2irTkeGi1nfw3ioFsmyiS57T3Ms";  // Google Maps Platform API
-String key = "5b3ce3597851110001cf624886c5e7f702de408ba27c7a71dc294d4b";  // Openroute Service API
+String key =
+    "5b3ce3597851110001cf624886c5e7f702de408ba27c7a71dc294d4b"; // Openroute Service API
 
 String web = "https://api.openrouteservice.org";
 String directionsService = "v2/directions/driving-car";
-String searchGeocoding   = "geocode/search";
+String searchGeocoding = "geocode/search";
 
 String locationWeb = "https://nominatim.openstreetmap.org";
-
-
 
 String weatherKey = "82f526738f7ff585d92ec4b470d8f1d3";
 String weatherWeb = "https://api.openweathermap.org";
 String oneCall = "data/3.0/onecall";
-
--- lib/service/account_reader.dart
+++ lib/service/account_reader.dart
@@ -4,43 +4,39 @@ import "dart:developer" as developer;
 import "package:http/http.dart" as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
 
-
 import '/general/constant.dart';
 import '/model/token_saver.dart';
 
-
-
 class AccountReader {
-
   // * Đăng ký tài khoản
   // * RETURN {
   // *   status: true
   // * }
-  Future< Map<String, dynamic> > register(String username, String phonenumber, String password) async {
+  Future<Map<String, dynamic>> register(
+      String username, String phonenumber, String password) async {
     // Đọc url để tìm tài khoản
     var response = await http.post(Uri.parse(Auth.register),
-                                      headers: { "Content-Type": "application/json; charset=UTF-8" },
-                                      body: jsonEncode({
-                                                        "phone": "+84$phonenumber",
-                                                        "password": password,
-                                                        "fullname": username,
-                                                      }));
+        headers: {"Content-Type": "application/json; charset=UTF-8"},
+        body: jsonEncode({
+          "phone": "$phonenumber",
+          "password": password,
+          "fullname": username,
+        }));
 
     try {
-      Map<String, dynamic> result = { "status": true };
+      Map<String, dynamic> result = {"status": true};
 
-      if ((response.statusCode == 200) || (response.statusCode == 201)) {}
-      else {
+      if ((response.statusCode == 200) || (response.statusCode == 201)) {
+      } else {
         developer.log("Failed HTTP when registering: ${response.statusCode}");
         result["status"] = false;
       }
       return result;
-
+    } catch (e) {
+      throw Exception(
+          "Failed code when trying to register an account, at saved_data.dart. Error type: ${e.toString()}");
     }
-    catch (e) { throw Exception("Failed code when trying to register an account, at saved_data.dart. Error type: ${e.toString()}"); }
   }
-
-
 
   // * Đăng nhập tài khoản
   // *
@@ -49,68 +45,68 @@ class AccountReader {
   // *   body: <Map>
   // * }
   // *
-  Future< Map<String, dynamic> > logIn(String phonenumber, String password) async {
-
-    Map<String, dynamic> result = { "status": false, "body": null };
+  Future<Map<String, dynamic>> logIn(
+      String phonenumber, String password) async {
+    Map<String, dynamic> result = {"status": false, "body": null};
     final response = await http.post(Uri.parse(Auth.login),
-                                    headers: { "Content-Type": "application/json; charset=UTF-8" },
-                                    body: jsonEncode({
-                                            "phone": "+84$phonenumber",
-                                            "password": password,
-                                          }));
+        headers: {"Content-Type": "application/json; charset=UTF-8"},
+        body: jsonEncode({
+          "phone": "$phonenumber",
+          "password": password,
+        }));
 
     try {
-      
       if ((response.statusCode == 200) || (response.statusCode == 201)) {
         developer.log("Successfully find the account's tokens.");
 
         var jsonVal = json.decode(response.body);
         developer.log(jsonVal.toString());
-        final accResponse = await http.get(Uri.parse(Customer.userInfo),
-                                            headers: { "Content-Type": "application/json; charset=UTF-8", "Authorization": "Bearer "+ jsonVal["accessToken"] });
-        
-        if ((accResponse.statusCode == 200) || (accResponse.statusCode == 201)) {
+        final accResponse =
+            await http.get(Uri.parse(Customer.userInfo), headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+          "Authorization": "Bearer " + jsonVal["accessToken"]
+        });
+
+        if ((accResponse.statusCode == 200) ||
+            (accResponse.statusCode == 201)) {
           developer.log("Successfully find the account's data.");
           result["body"] = json.decode(utf8.decode(accResponse.bodyBytes));
-          await TokenSaver().save(jsonVal["accessToken"], jsonVal["refreshToken"]);
+          await TokenSaver()
+              .save(jsonVal["accessToken"], jsonVal["refreshToken"]);
           result["status"] = true;
+        } else {
+          developer.log(
+              "Failed HTTP when accessing the accessToken during logging in: ${accResponse.statusCode}");
         }
-        else {
-          developer.log("Failed HTTP when accessing the accessToken during logging in: ${accResponse.statusCode}");
-        }
-      }
-      else {
+      } else {
         developer.log("Failed HTTP when logging in: ${response.statusCode}");
       }
       return result;
+    } catch (e) {
+      throw Exception(
+          "Failed code when trying to log in, at saved_data.dart. Error type: ${e.toString()}");
     }
-    catch (e) { throw Exception("Failed code when trying to log in, at saved_data.dart. Error type: ${e.toString()}"); }
   }
 
-
-
-  Future< Map<String, dynamic> > logOut() async {
-
-    Map<String, dynamic> result = { "status": false, "body": null };
+  Future<Map<String, dynamic>> logOut() async {
+    Map<String, dynamic> result = {"status": false, "body": null};
     final response = await http.get(Uri.parse(Auth.logout), headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-        "Authorization": "Bearer "+ await TokenSaver().loadAccessToken()
-      });
+      "Content-Type": "application/json; charset=UTF-8",
+      "Authorization": "Bearer " + await TokenSaver().loadAccessToken()
+    });
 
     try {
-      
       if ((response.statusCode == 200) || (response.statusCode == 201)) {
         result["status"] = true;
-      }
-      else {
+      } else {
         developer.log("Failed HTTP when logging out: ${response.statusCode}");
       }
       return result;
+    } catch (e) {
+      throw Exception(
+          "Failed code when trying to log in, at saved_data.dart. Error type: ${e.toString()}");
     }
-    catch (e) { throw Exception("Failed code when trying to log in, at saved_data.dart. Error type: ${e.toString()}"); }
   }
-
-
 
   // *
   // * Kiểm tra token ở mỗi lần khai báo, đăng nhập, v.v...
@@ -120,35 +116,36 @@ class AccountReader {
     return (token.isNotEmpty) && !JwtDecoder.isExpired(token);
   }
 
-
-
   // * Lấy token mới
   // *
   // * RETURN: <String> accessToken
   // *
-  Future< String > getNewTokens() async {
-
-    final response = await http.get(Uri.parse(Auth.refresh),
-                                    headers: { "Content-Type": "application/json; charset=UTF-8", "Authorization": "Bearer "+ await TokenSaver().loadRefreshToken() });
+  Future<String> getNewTokens() async {
+    final response = await http.get(Uri.parse(Auth.refresh), headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      "Authorization": "Bearer " + await TokenSaver().loadRefreshToken()
+    });
 
     try {
       if ((response.statusCode == 200) || (response.statusCode == 201)) {
         developer.log("Successfully find the new tokens.");
         final jsonVal = json.decode(response.body);
-        await TokenSaver().save(jsonVal["accessToken"], jsonVal["refreshToken"]);
+        await TokenSaver()
+            .save(jsonVal["accessToken"], jsonVal["refreshToken"]);
         return jsonVal["accessToken"];
-      }
-      else {
-        developer.log("Failed HTTP when getting new Tokens: ${response.statusCode}");
-        developer.log("Refresh token: ${await TokenSaver().loadRefreshToken()}");
+      } else {
+        developer
+            .log("Failed HTTP when getting new Tokens: ${response.statusCode}");
+        developer
+            .log("Refresh token: ${await TokenSaver().loadRefreshToken()}");
         // TokenSaver().clear();
         return "";
       }
+    } catch (e) {
+      throw Exception(
+          "Failed code when trying to log in, at saved_data.dart. Error type: ${e.toString()}");
     }
-    catch (e) { throw Exception("Failed code when trying to log in, at saved_data.dart. Error type: ${e.toString()}"); }
   }
-
-
 
   // * Chạy hàm
   // *
@@ -157,11 +154,12 @@ class AccountReader {
   // *   body: <Map>
   // * }
   // *
-  Future< Map<String, dynamic> > toggleFunction(Future Function(String token) toggleHttp, String functionDetails, { bool callExpired = false }) async {
-
+  Future<Map<String, dynamic>> toggleFunction(
+      Future Function(String token) toggleHttp, String functionDetails,
+      {bool callExpired = false}) async {
     if (!callExpired) developer.log("Trying to compile $functionDetails.");
 
-    Map<String, dynamic> result = { "status": false, "body": null };
+    Map<String, dynamic> result = {"status": false, "body": null};
 
     String accessToken = await TokenSaver().loadAccessToken();
     if (accessToken.isEmpty) {
@@ -171,35 +169,38 @@ class AccountReader {
     }
 
     try {
-      final response = await toggleHttp(accessToken).timeout(const Duration(seconds: 10));
+      final response =
+          await toggleHttp(accessToken).timeout(const Duration(seconds: 10));
       switch (response.statusCode) {
-
-        case 200: case 201:
+        case 200:
+        case 201:
           result["status"] = true;
           result["body"] = json.decode(utf8.decode(response.bodyBytes));
           break;
-        
+
         case 401:
           if (!callExpired) {
             developer.log("[Access token] Expired");
             await getNewTokens();
-            result = await toggleFunction(toggleHttp, functionDetails, callExpired: true);
-          }
-          else {
+            result = await toggleFunction(toggleHttp, functionDetails,
+                callExpired: true);
+          } else {
             developer.log("[Access token & Refresh token] Expired both!");
           }
           break;
 
         default:
-          developer.log("Failed HTTP when compiling $functionDetails. Error HTTP: ${response.statusCode}.");
+          developer.log(
+              "Failed HTTP when compiling $functionDetails. Error HTTP: ${response.statusCode}.");
           developer.log("Response body: ${response.body}");
           break;
       }
-    }
-    on TimeoutException catch (e, s) {
+    } on TimeoutException catch (e, s) {
       developer.log("The request for $functionDetails took too long. S: $s");
+    } catch (e) {
+      throw Exception(
+          "Failed code when trying to compile $functionDetails, at saved_data.dart. Error type: ${e.toString()}");
     }
-    catch (e) { throw Exception("Failed code when trying to compile $functionDetails, at saved_data.dart. Error type: ${e.toString()}"); }
 
     return result;
   }
